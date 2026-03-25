a376ee6aa01eded557725de7ce73d643caf21a88
Bruno Lopes de Paiva <brunolopesdepaiva@gmail.com>
2026-03-25 10:32:50 -0300

    0.0.15

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Viarondon | Chamados",
+  title: "HelpMe | Sistema de chamados ViaRondon",
   description: "Sistema interno de chamados da ViaRondon Concessionária de Rodovia S.A",
 };
 
